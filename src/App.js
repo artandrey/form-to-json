f39779e005582edb4f34c5aab43f0ad8a0918d0c
Artandrey <artandrey7777@gmail.com>
2022-11-10 11:34:38 +0200

    created functional components from class components

--- src/App.js
+++ src/App.js
@@ -1,58 +1,52 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import Form from './Form';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { resultJson: '' };
-        this.handleFormSubmit = this.handleFormSubmit.bind(this);
-        this.defaultValue = `{
-          "blocks": [
-           {
-            "key": "1kdsb",
-            "text": "Default text from App component",
-            "type": "unstyled",
-            "depth": 0,
-            "inlineStyleRanges": [],
-            "entityRanges": [],
-            "data": {}
-           },
-           {
-            "key": "2ipln",
-            "text": "",
-            "type": "unstyled",
-            "depth": 0,
-            "inlineStyleRanges": [],
-            "entityRanges": [],
-            "data": {}
-           },
-           {
-            "key": "2glph",
-            "text": "Formetted and with emojis 👍",
-            "type": "unstyled",
-            "depth": 0,
-            "inlineStyleRanges": [],
-            "entityRanges": [],
-            "data": {}
-           }
-          ],
-          "entityMap": {}
-         }`;
-    }
-    handleFormSubmit(json) {
-        this.setState({ resultJson: json });
-    }
-    render() {
-        return (
-            <div>
-                <Form
-                    content={this.defaultValue}
-                    handleSubmit={this.handleFormSubmit}
-                />
-                <pre>{this.state.resultJson}</pre>
-            </div>
-        );
-    }
-}
+const App = () => {
+    const defaultValue = `{
+    "blocks": [
+     {
+      "key": "1kdsb",
+      "text": "Default text from App component",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+     },
+     {
+      "key": "2ipln",
+      "text": "",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+     },
+     {
+      "key": "2glph",
+      "text": "Formetted and with emojis 👍",
+      "type": "unstyled",
+      "depth": 0,
+      "inlineStyleRanges": [],
+      "entityRanges": [],
+      "data": {}
+     }
+    ],
+    "entityMap": {}
+   }`;
+    const [jsonValue, setJsonValue] = useState(defaultValue);
+    const handleFormSubmit = useCallback(
+        (json) => {
+            setJsonValue(json);
+        },
+        [setJsonValue]
+    );
+    return (
+        <div>
+            <Form content={jsonValue} onSubmit={handleFormSubmit} />
+            <pre>{jsonValue}</pre>
+        </div>
+    );
+};
 
 export default App;
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,52 +1,41 @@
-import React, { Component } from 'react';
-import { EditorState, Editor, convertToRaw, convertFromRaw } from 'draft-js';
+import React, { useCallback, useState } from 'react';
+import { EditorState, Editor, convertFromRaw } from 'draft-js';
 import 'draft-js/dist/Draft.css';
 import getRawJSONStringFromEditorState from './converters/raw-json-from-draft-state';
 
-export default class Form extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { editorState: EditorState.createEmpty() };
-
-        const { content, handleSubmit } = props;
-
-        this.onSubmit = this.onSubmit.bind(this);
-
-        this.handleSubmit = handleSubmit ? handleSubmit : null;
-
-        if (content) {
-            this.state.editorState = EditorState.createWithContent(
-                convertFromRaw(JSON.parse(content))
-            );
-        } else {
-            this.state.editorState = EditorState.createEmpty();
-        }
-    }
-
-    onChange = (editorState) => {
-        this.setState({
-            editorState,
-        });
-    };
-
-    onSubmit(event) {
-        event.preventDefault();
-        const json = getRawJSONStringFromEditorState(this.state.editorState);
-        this.handleSubmit && this.handleSubmit(json);
-    }
-
-    render() {
-        return (
-            <form onSubmit={this.onSubmit}>
-                <h1>Editor</h1>
-                <div style={{ border: 'solid 1px black' }}>
-                    <Editor
-                        editorState={this.state.editorState}
-                        onChange={this.onChange}
-                    />
-                </div>
-                <button>Save</button>
-            </form>
+const initEditorState = (content) => {
+    if (content) {
+        return EditorState.createWithContent(
+            convertFromRaw(JSON.parse(content))
         );
+    } else {
+        return EditorState.createEmpty();
     }
-}
+};
+
+const Form = ({ content, onSubmit }) => {
+    const [editorValue, setEditorValue] = useState(initEditorState(content));
+    const handleFormSubmit = useCallback(
+        (event) => {
+            event.preventDefault();
+            const json = getRawJSONStringFromEditorState(editorValue);
+            onSubmit && onSubmit(json);
+        },
+        [editorValue, onSubmit]
+    );
+
+    return (
+        <form onSubmit={handleFormSubmit}>
+            <h1>Editor</h1>
+            <div style={{ border: 'solid 1px black' }}>
+                <Editor
+                    editorState={this.state.editorState}
+                    onChange={setEditorValue}
+                />
+            </div>
+            <button>Save</button>
+        </form>
+    );
+};
+
+export default Form;
